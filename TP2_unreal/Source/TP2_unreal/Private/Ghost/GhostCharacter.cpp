12beb3846ef0d608ab0fd66d2e8271418055935a
Coralie <coralie.tremblay12@icloud.com>
2024-10-05 14:52:34 -0400

    Classe Parent Entity (Enfants : Player & ghost)

--- TP2_unreal/Source/TP2_unreal/Private/Ghost/GhostCharacter.cpp
+++ TP2_unreal/Source/TP2_unreal/Private/Ghost/GhostCharacter.cpp
@@ -2,31 +2,18 @@
 
 
 #include "Ghost/GhostCharacter.h"
-#include "Components/BoxComponent.h"
-#include "GameFramework//FloatingPawnMovement.h"
 
 // Sets default values
 AGhostCharacter::AGhostCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	BoxCollision = CreateDefaultSubobject<UBoxComponent>("BoxCollision");
-	RootComponent = BoxCollision;
-
-	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
-	StaticMesh->SetupAttachment(BoxCollision);
-
-	MovementComponent = CreateDefaultSubobject<UPawnMovementComponent, UFloatingPawnMovement>(TEXT("MovementComponent"));
-	MovementComponent->UpdatedComponent = BoxCollision;
-
 }
 
 // Called when the game starts or when spawned
 void AGhostCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -44,21 +31,4 @@ void AGhostCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-}
-
-void AGhostCharacter::MoveTo(FVector Location)
-{
-	MovementComponent->AddInputVector(Location);
-}
-
-FVector AGhostCharacter::Seek(FVector Target)
-{
-	FVector DesiredVelocity = Target - GetActorLocation();
-	DesiredVelocity.Normalize();
-	DesiredVelocity *= MovementComponent->GetMaxSpeed();
-
-	FVector Steering = DesiredVelocity - MovementComponent->Velocity;
-	Steering = Steering.GetClampedToMaxSize(MovementComponent->GetMaxSpeed());
-
-	return Steering;
 }
